4467d5695dfa59bb976d490cddf3d01076462844
Erik Feed Wie <98807054+ErikFW@users.noreply.github.com>
2026-09-16 09:50:43 +0200

    Add header comments to history_timestamp.sh
    Added header comments to provide context and metadata.

--- history_timestamp.sh
+++ history_timestamp.sh
@@ -1,1 +1,9 @@
+#!/bin/sh
+# --------------------------------------------------
+# Purpose: Set up timestamping of the history log
+# Author: Erik Feed Wie
+# Date: September 2026
+# Version: 1.0.1
+# --------------------------------------------------
+
 export HISTTIMEFORMAT="%Y.%m.%d %H:%M:%S %Z UTC%z "
